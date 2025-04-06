8ecd68e63aa7fe990be71d91ff19f84c9fec4f0b
Mau <natanielmauricioarapaestrada@gmail.com>
2025-04-05 21:27:49 -0400

    quitando puerto 3307

--- Tareas/Tarea parcial/Seccion bd/25/conexion.php
+++ Tareas/Tarea parcial/Seccion bd/25/conexion.php
@@ -1,5 +1,5 @@
 <?php
-$conexion = new mysqli("localhost", "root", "", "bd_tienda",3307);
+$conexion = new mysqli("localhost", "root", "", "bd_tienda");
 if ($conexion->connect_error) {
     die("Conexión fallida: " . $conexion->connect_error);
 }
